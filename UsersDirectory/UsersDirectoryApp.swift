d39329562d2306ce5a28d79ccb47f3ae6e2369bb
Dan Rudolf <danrudolf@gmail.com>
2023-08-22 15:00:44 -0500

    Initial Commit

---
+++ UsersDirectory/UsersDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersDirectoryApp.swift
+//  UsersDirectory
+//
+//  Created by Dan on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct UsersDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
